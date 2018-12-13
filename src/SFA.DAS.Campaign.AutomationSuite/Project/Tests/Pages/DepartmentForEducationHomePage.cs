0803cba097d1c01eff23c58e9a351fc5c6a663f9
Siji Odunlami <sijiodunlami@gmail.com>
2018-12-13 15:08:39 +0000

    Initial commit with skeleton framework

---
+++ src/SFA.DAS.Campaign.AutomationSuite/Project/Tests/Pages/DepartmentForEducationHomePage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SFA.DAS.Campaign.AutomationSuite.Project.Framework.Helpers;
+using SFA.DAS.Campaign.AutomationSuite.Project.Tests.TestSupport;
+using OpenQA.Selenium;
+
+namespace SFA.DAS.Campaign.AutomationSuite.Project.Tests.Pages
+{
+    class DepartmentForEducationHomePage : BasePage
+    {
+        private static String PAGE_TITLE = "Department\r\nfor Education";
+
+        public DepartmentForEducationHomePage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+    }
+}
